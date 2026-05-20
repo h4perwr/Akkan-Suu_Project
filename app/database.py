9a18c2f2fc71eeaec19a394accd637a501e06d68
Alina Massanova <alinamassanova42@gmail.com>
2026-05-20 12:26:25 +0600

    fixed bugs

--- app/database.py
+++ app/database.py
@@ -4,21 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 
-# Создаем движок подключения
 engine = create_engine(settings.DATABASE_URL)
 
-# Создаем фабрику сессий. Каждая сессия — это отдельная транзакция
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Базовый класс, от которого будут исследоваться все наши таблицы
 Base = declarative_base()
 
-# Специальная функция-зависимость (Dependency Injection) для FastAPI.
-# Она открывает сессию базы данных для каждого запроса и закрывает её после ответа.
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
