eb105a8f6f5b9e36a4e29f253b5d79ce36d35167
h4per <h4peryt@gmail.com>
2026-05-24 19:50:47 +0600

    fixed vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  allowedHosts: [
+  server: {
+      port: 3000,
+      host: '0.0.0.0',
+      
+      allowedHosts: [
         'compassionate-radiance-production-b389.up.railway.app'
       ]
+    }
 })
